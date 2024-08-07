789b943c15618e28e999a7c51dbf71888e231e91
Masoom1307 <c3080045@hallam.shu.ac.uk>
2024-08-07 19:38:25 +0100

    Linked base.html

--- lmsreporting/urls.py
+++ lmsreporting/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-app_name = 'itreporting' 
+app_name = 'lmsreporting' 
 
 
 urlpatterns = [ 
--- lmsreporting/views.py
+++ lmsreporting/views.py
@@ -4,18 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request): 
 
-    return render(request, 'lmsreporting/home.html')
+    return render(request, 'lmsreporting/home.html', {'title': 'Welcome'})
 
 def about(request): 
 
-    return render(request, 'lmsreporting/about.html')
+    return render(request, 'lmsreporting/about.html', {'title': 'About'})
 
 
 def contact(request): 
 
-    return render(request, 'lmsreporting/contact.html')
+    return render(request, 'lmsreporting/contact.html', {'title': 'Contact'})
 
 
 def modules(request): 
 
-    return render(request, 'lmsreporting/modules.html')
+    return render(request, 'lmsreporting/modules.html', {'title': 'Modules'})
