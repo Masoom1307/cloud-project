f556a3fa8b06e37cf1e4d6086a277301d5d4ea49
Masoom1307 <c3080045@hallam.shu.ac.uk>
2024-08-08 00:42:06 +0100

    profile functionality added

--- lmsapp/settings.py
+++ lmsapp/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lmsreporting.apps.LmsreportingConfig',
-       
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',    
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'lmsreporting:home'
+LOGIN_URL = 'login' 
