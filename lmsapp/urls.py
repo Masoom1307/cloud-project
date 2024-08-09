4131a26cafb7630f532bbf3f4fc9b76b91de6966
Masoom1307 <c3080045@hallam.shu.ac.uk>
2024-08-09 02:47:00 +0100

    Created User Profile

--- lmsapp/urls.py
+++ lmsapp/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.contrib.auth import views as auth_views
 from users import views as vw
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -29,3 +31,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile', vw.profile, name = 'profile'), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
