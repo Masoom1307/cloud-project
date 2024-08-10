c4b3be1cb52fe2087e5f84111b5742401bd8c009
Masoom1307 <c3080045@hallam.shu.ac.uk>
2024-08-10 13:18:18 +0100

    Practice Model Created

--- lmsreporting/urls.py
+++ lmsreporting/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
 app_name = 'lmsreporting' 
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [ 
@@ -9,5 +10,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('modules/', views.modules, name='modules'),
+    path('issues/', PostListView.as_view(), name = 'report'),
+    path('issues/<int:pk>', PostDetailView.as_view(), name = 'issue-detail'),
+    path('issues/new', PostCreateView.as_view(), name = 'issue-create'),
+    path('issues/<int:pk>/update/', PostUpdateView.as_view(), name = 'issue-update'),
+    path('issues/<int:pk>/delete/', PostDeleteView.as_view(), name = 'issue-delete'),
 
 ]
--- lmsreporting/views.py
+++ lmsreporting/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Issue
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Issue
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
+from django.views.generic.edit import DeleteView
 
 # Create your views here.
 def home(request): 
@@ -19,3 +25,59 @@ def contact(request):
 def modules(request): 
 
     return render(request, 'lmsreporting/modules.html', {'title': 'Modules'})
+
+
+def report(request):
+   
+    daily_report = {'issues': Issue.objects.all(), 'title': 'Issues Reported'}
+    return render(request, 'lmsreporting/report.html', daily_report)
+
+class PostListView(ListView):
+    model = Issue
+    ordering = ['-date_submitted']
+    template_name = 'lmsreporting/report.html'
+    context_object_name = 'issues'
+    paginate_by = 10  # Optional pagination
+
+class PostDetailView(DetailView):
+    model = Issue
+    template_name = 'lmsreporting/issue_detail.html'
+
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+
+    model = Issue
+    fields = ['type', 'room', 'urgent', 'details']
+
+    def form_valid(self, form): 
+
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
+
+    model = Issue
+
+    fields = ['type', 'room', 'details']
+    
+    def test_func(self):
+
+        issue = self.get_object()
+
+        return self.request.user == issue.author
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+
+    model = Issue
+
+    success_url = '/issues'
+    
+    def test_func(self):
+
+        issue = self.get_object()
+
+        return self.request.user == issue.author
+
+
+
+
